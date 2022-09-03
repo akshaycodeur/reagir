30d6ca994ba646552a314776400261896dd63ccf
Ashcode <akshaycodeur@gmail.com>
2022-09-02 23:34:24 -0400

    dynamic data

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let title = 'This is a new title'
   return (
     <div className="App">
       <div className="content">
-        <h1>This is the heading</h1>
+        <h1>Heading : {title}</h1>
       </div>
     </div>
   );
