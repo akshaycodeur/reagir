3bdbcd958d0a72b41bd877e080a683a681f11a45
Ashcode <akshaycodeur@gmail.com>
2022-09-02 23:38:49 -0400

    created navbar component and imported it

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './Navbar';
 
 function App() {
   let title = 'This is a new title'
   return (
     <div className="App">
+      <Navbar />
       <div className="content">
         <h1>Heading : {title}</h1>
       </div>
