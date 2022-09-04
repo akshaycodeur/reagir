ae106cd715a90d93ea25e03d0c5c83dc89fe90c9
Ashcode <akshaycodeur@gmail.com>
2022-09-03 23:59:36 -0400

    edits

--- src/Home.js
+++ src/Home.js
@@ -1,21 +1,19 @@
 const Home = () => {
 
-    const handleClick = () =>{
-        console.log('You just clicked on the button');
-    }
+    let name = 'John Smith';
 
-    const handleClickVar = (name) => {
-        console.log("Hello " + name)
+    const handleChangeName = () => {
+        console.log(name);
+        name = "Jane Doe";
+        console.log(name);
     }
 
     return ( 
         <div className="home">
             <h2>Homepage</h2>
-            <button onClick = {handleClick}>Click me</button>
-            <h4>Now the anonymous function</h4>
-            <button onClick={()=>{handleClickVar('Akilou');}}>This click uses a variable</button>
-            <h4>Another way to write the above function is as follows</h4>
-            <button onClick={()=>handleClickVar('Dorothee')}>This is another way to execute the same function as the previous one</button>
+            <hr />
+            <h5>Will clicking on the button change the value of the variable <strong>name</strong> ?</h5>
+            <button onClick = {handleChangeName}>Click me</button>
         </div>
      );
 }
