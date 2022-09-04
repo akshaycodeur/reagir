a05282ed6a1840ce2757d86d319cfcb960b5994d
Ashcode <akshaycodeur@gmail.com>
2022-09-03 23:43:07 -0400

    alternate way for click event while passing variable

--- src/Home.js
+++ src/Home.js
@@ -13,9 +13,9 @@ const Home = () => {
             <h2>Homepage</h2>
             <button onClick = {handleClick}>Click me</button>
             <h4>Now the anonymous function</h4>
-            <button onClick={()=>{
-                handleClickVar('Akilou');
-            }}>This click uses a variable</button>
+            <button onClick={()=>{handleClickVar('Akilou');}}>This click uses a variable</button>
+            <h4>Another way to write the above function is as follows</h4>
+            <button onClick={()=>handleClickVar('Dorothee')}>This is another way to execute the same function as the previous one</button>
         </div>
      );
 }
