36646fbb931f3553360ee53786710a0891f72d7b
Ashcode <akshaycodeur@gmail.com>
2022-09-03 23:40:36 -0400

    click events passing variables

--- src/Home.js
+++ src/Home.js
@@ -4,10 +4,18 @@ const Home = () => {
         console.log('You just clicked on the button');
     }
 
+    const handleClickVar = (name) => {
+        console.log("Hello " + name)
+    }
+
     return ( 
         <div className="home">
             <h2>Homepage</h2>
             <button onClick = {handleClick}>Click me</button>
+            <h4>Now the anonymous function</h4>
+            <button onClick={()=>{
+                handleClickVar('Akilou');
+            }}>This click uses a variable</button>
         </div>
      );
 }
