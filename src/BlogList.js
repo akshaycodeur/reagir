ce485d9eeb14f31228379fb429e0a5872da78b60
Ashcode <akshaycodeur@gmail.com>
2022-10-07 10:21:30 -0400

    restore point : Basic props, components and json server being activated

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,4 +1,4 @@
-const BlogList = ({blogs,people,title,theirTitle, pokemon,handleDelete}) => {
+const BlogList = ({blogs,title}) => {
     
     return ( 
         <div className="blog-list">
@@ -9,27 +9,10 @@ const BlogList = ({blogs,people,title,theirTitle, pokemon,handleDelete}) => {
                 <div className="blog-preview" key={blogpost.id}>
                     <h2>{blogpost.title}</h2>
                     <p>Written by {blogpost.author}</p>
-                    <button className="deletebutton" onClick={ () => handleDelete(blogs.id)} >Delete Record</button>
-                    <hr />
+                    <hr  className="separator"/>
                 </div>
             ))}
-            <hr />
-            <hr />
-            <h1>{theirTitle}</h1>
-            {people.map((peoplesss) => (
-                <div className="blog-preview" key={peoplesss.id}>
-                    <h2>{peoplesss.name}</h2>
-                </div>
-            ))}
-            <hr />
-            <hr />
-            <hr />
-            <hr />
-            {pokemon.map((poketmon) => (
-                <div className="blog-preview" key={poketmon.id}>
-                    <h3>{poketmon.pokename}</h3>
-                </div>
-            ))}
+
            
         </div>
      );
--- src/Home.js
+++ src/Home.js
@@ -9,35 +9,12 @@ const Home = () => {
         {title:'Day One In Oshawa', body:'lorem ipsum...', author:'Red Foreman', id:3}
     ]);
 
-    const [people,setPeoplexxx] = useState([
-        {name: 'Ash', age: 13, id:1},
-        {name: 'Gary', age: 33, id:2},
-        {name: 'Red', age: 23, id:3}
 
-    ]);
 
-    const listTitle = "The Pokemon List";
-    const [pokemon, setPokemons] = useState([
-        {pokename: 'Pikachu', move1: 'Tackle', move2:'Lightning', move3:'Growl', move4:'Quick Attack', id:1},
-        {pokename: 'Ratata', move1: 'Tackle', move2:'Growl', move3:'Leer', move4:'Quick Attack', id:2},
-        {pokename: 'Hinoarashi', move1: 'Scratch', move2:'Punch', move3:'Ember', move4:'Leer', id:3},
-        {pokename: 'Charmander', move1: 'Scratch', move2:'Punch', move3:'Ember', move4:'Leer', id:4},
-        {pokename: 'Squirtle', move1: 'Tackle', move2:'Growl', move3:'Bubble Blast', move4:'Water Gun', id:5}
-    ]);
-    
-    const theirTitle = 'Pokemon Trainers';
-
-    const handleDelete = (id) => {
-        console.log('You clicked on the delete button');
-    };
 
     return ( 
         <div className='home'>
-            <BlogList blogs={blogs} title='Maitre des sortileges' people={people} theirTitle={theirTitle} pokemon = {pokemon} eventDelete={handleDelete} />
-            <hr />
-            <hr />
-            <hr />
-            <BlogList pokemon={pokemon.filter((pokemon) => pokemon.move1==='Scratch')} title='Maitre des sortileges2' people={people} theirTitle={theirTitle} blogs = {blogs} eventDelete={handleDelete} />
+            <BlogList blogs={blogs} title='Maitre des sortileges' />
         </div>
      );
 }
