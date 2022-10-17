053626d2ec7fd16f0026fb3a9ea2d0360c843a70
Ashcode <akshaycodeur@gmail.com>
2022-10-17 17:11:01 -0400

    Catch errors

--- src/Home.js
+++ src/Home.js
@@ -6,16 +6,27 @@ const Home = () => {
 
     const [blogs,setBlogs] = useState(null);
     const [isPending,setIsPending] = useState(true);
+    const [error,setError] = useState(null);
 
 
     useEffect(() => {
       setTimeout(() => {
         fetch('http://localhost:8001/blogs')
           .then(res => {
+            console.log(res);
+            if(!res.ok){
+              throw Error('Could not fetch the data');
+            }
             return res.json();
           })
           .then(data => {
             setBlogs(data);
+            setIsPending(false);
+            setError(null);
+          })
+          .catch( err => {
+            console.log(err.message);
+            setError(err.message);
             setIsPending(false);
           })
       }, 1000)
@@ -23,6 +34,7 @@ const Home = () => {
 
     return ( 
         <div className='home'>
+          {error && <div>{error}</div>}
             { isPending && <div>Loading</div> }
             {blogs && <BlogList blogs={blogs} title='Welcome to the pokedex 2' />}
         </div>
