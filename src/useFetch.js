6f4973e813953c60e57b6556cd4a0732eab385b0
Ashcode <akshaycodeur@gmail.com>
2022-10-18 23:22:34 -0400

    Unable to create custom hook. Installed react router

--- src/useFetch.js
+++ src/useFetch.js
@@ -6,29 +6,33 @@ const useFetch = (url) => {
 
     const [data, setData] = useState(null);
     const [isPending,setIsPending] = useState(true);
-    const [networkError, setNetworkError] = useState(null);
+    const [error, setError] = useState(null);
     const sectionOneTitle = 'Pokemon Lists';
 
-    useEffect(()=>{
-        fetch(url)
-        .then(res =>{
-            if(!res.ok){
-                throw Error('Could not fetch the date for that resources')
-            }
-            return res.json();
-        }) 
-        .then((data)=>{
-            setData(data);
-            setIsPending(false);
-            setNetworkError(null);
-        })
-        .catch( e => {
-            setNetworkError(e.message)
-            setIsPending(false);
-        })
-    },[url]);
+    useEffect(() => {
+        setTimeout(() => {
+          fetch(url)
+            .then(res => {
+              console.log(res);
+              if(!res.ok){
+                throw Error('Could not fetch the data');
+              }
+              return res.json();
+            })
+            .then(data => {
+              setData(data);
+              setIsPending(false);
+              setError(null);
+            })
+            .catch( err => {
+              console.log(err.message);
+              setError(err.message);
+              setIsPending(false);
+            })
+        }, 1000)
+        }, [url])
 
-    return{ data, isPending, networkError, sectionOneTitle}
+    return{ data, isPending, error, sectionOneTitle}
 
 }
 
