918bd3662b81c14f8fbc6d40cd77091eda17a171
Ashcode <akshaycodeur@gmail.com>
2022-10-20 15:44:24 -0400

    creating form in react

--- src/CreatePokemon.js
+++ src/CreatePokemon.js
@@ -1,7 +1,30 @@
+import { useState } from "react";
+
 const CreatePokemon = () => {
+
+    const [pokemonname,setPokemonname] = useState('Enter Pokemon Name');
+    const [pokeType,setPokeType] = useState('Enter Pokemon Type #1');
+
     return (  
         <div className="createPokemon">
             <h1>Add new pokemon entry</h1>
+            <form action="">
+                <label>Pokemon Name</label>
+                <input 
+                type="text"
+                required 
+                value={pokemonname}
+                onChange={(e) => setPokemonname(e.target.value)}
+                />
+                 <label>Pokemon type</label>
+                <input 
+                type="text"
+                required 
+                value={pokeType}
+                onChange={(e)=>setPokeType(e.target.value)}
+                />
+                <button>Add Pokemon</button>
+            </form>
         </div>
     );
 }
