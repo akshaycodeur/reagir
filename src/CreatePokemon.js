058e3c2b2b7cbc51992f3ac4ee1c07194e211748
Ashcode <akshaycodeur@gmail.com>
2022-10-21 15:23:26 -0400

    Adding entry to pokedex

--- src/CreatePokemon.js
+++ src/CreatePokemon.js
@@ -1,12 +1,27 @@
 import { useState } from "react";
+import { useHistory } from "react-router-dom";
 
 const CreatePokemon = () => {
 
-    const [pokemonname,setPokemonname] = useState('Enter Pokemon Name');
-    const [pokeType,setPokeType] = useState('Enter Pokemon Type #1');
+    const [name,setName] = useState('Enter Pokemon Name');
+    const [types,setTypes] = useState('Enter Pokemon Type #1');
+    const [description,SetDescription] = useState('Enter Pokemon Description');
+
+    const history = useHistory();
 
     const handleSubmit = (e) => {
+        e.preventDefault();
+        const pokemon = {name,types,description};
+        console.log(pokemon);
 
+        fetch('http://localhost:8001/pokemon', {
+            method: 'POST',
+            headers: {'Content-Type':'application/json'},
+            body: JSON.stringify(pokemon)
+        }).then(() => {
+            console.log('New Pokemon Added');
+            history.push('/');
+        })
     }
 
     return (  
@@ -17,15 +32,22 @@ const CreatePokemon = () => {
                 <input 
                 type="text"
                 required 
-                value={pokemonname}
-                onChange={(e) => setPokemonname(e.target.value)}
+                value={name}
+                onChange={(e) => setName(e.target.value)}
                 />
                  <label>Pokemon type</label>
                 <input 
                 type="text"
                 required 
-                value={pokeType}
-                onChange={(e)=>setPokeType(e.target.value)}
+                value={types}
+                onChange={(e)=>setTypes(e.target.value)}
+                />
+                 <label>Pokemon Description</label>
+                <textarea 
+                type="text"
+                required 
+                value={description}
+                onChange={(e)=>SetDescription(e.target.value)}
                 />
                 <button>Add Pokemon</button>
             </form>
